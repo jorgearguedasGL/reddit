f96c9cce3bbcffe38549c9bbe7d9b9d6483c9f16
jorge.arguedas <jorge.arguedas@homeadvisor.com>
2021-05-18 10:09:35 -0600

    Modification to main.go file

--- main.go
+++ main.go
@@ -1,23 +1,59 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	resp, err := http.Get("http://reddit.com/r/golang.json")
-	//nil is the zero value for built-in error type
+	items, err := Get("golang")
 	if err != nil {
 		log.Fatal(err)
 	}
+	for _, item := range items {
+		fmt.Printf(item.Title)
+	}
+
+}
+
+func Get(reddit string) ([]Item, error) {
+	url := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	//guarantee that we clean up after the HTTP request
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		log.Fatal(resp.Status)
+		return nil, errors.New(resp.Status)
 	}
-	_, err = io.Copy(os.Stdout, resp.Body)
+	r := new(Response)
+	err = json.NewDecoder(resp.Body).Decode(r)
 	if err != nil {
 		log.Fatal(err)
+	}
+	fmt.Println("Printing JSON")
+	items := make([]Item, len(r.Data.Children))
+	//for _, child := range r.Data.Children {
+	for i, child := range r.Data.Children {
+		items[i] = child.Data
+	}
+	return items, nil
+
+}
+
+type Item struct {
+	Title string
+	URL   string
+}
+
+type Response struct {
+	Data struct {
+		Children []struct {
+			Data Item
+		}
 	}
 }
